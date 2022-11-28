823e62987d8c9019c2c837107480b4559f9ed500
gangye <2664725818@qq.com>
2022-11-28 21:42:03 +0800

    20221128|设计模式——支付案例-单例模式优化

--- src/main/java/com/csrcb/design/pay/strategyFactory/StrategyFactory.java
+++ src/main/java/com/csrcb/design/pay/strategyFactory/StrategyFactory.java
@@ -3,6 +3,9 @@ package com.csrcb.design.pay.strategyFactory;
 import com.csrcb.design.pay.strategy.PayStrategy;
 import com.csrcb.design.pay.strategyEnum.StrategyEnum;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @ClassName StrategyFactory
  * @Description 工厂模式  工厂类依靠策略枚举返回策略类
@@ -10,14 +13,18 @@ import com.csrcb.design.pay.strategyEnum.StrategyEnum;
  * @Date 2022/11/21
  */
 public class StrategyFactory {
+    private static final Map<String, PayStrategy> strategyMaps = new ConcurrentHashMap<>();
     public static PayStrategy getPayStrategy(StrategyEnum strategyEnum){
-        PayStrategy payStrategy = null;
-        try {
-            payStrategy = (PayStrategy) Class.forName(strategyEnum.getValue()).newInstance();
-        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            //异常
-            throw new RuntimeException(e);
+        PayStrategy payStrategy = strategyMaps.get(strategyEnum.getValue());
+        if (null == payStrategy) {
+            try {
+                // 每次获取都需要进行一次反射。需要优化调整(结合单例模式)
+                payStrategy = (PayStrategy) Class.forName(strategyEnum.getValue()).newInstance();
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                //异常
+                throw new RuntimeException(e);
+            }
         }
-        return null;
+        return payStrategy;
     }
 }
